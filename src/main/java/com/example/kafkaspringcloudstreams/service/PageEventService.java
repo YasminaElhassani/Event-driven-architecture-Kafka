2d33c2ff1c7687e898741565525b750f946010f2
yasmina <[ELHASSANI_YASMINA@emsi-edu.ma]>
2024-02-07 23:01:01 +0100

    forth commit

--- src/main/java/com/example/kafkaspringcloudstreams/service/PageEventService.java
+++ src/main/java/com/example/kafkaspringcloudstreams/service/PageEventService.java
@@ -5,9 +5,12 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.kstream.TimeWindows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 import java.util.Random;
 import java.util.function.Consumer;
@@ -51,14 +54,38 @@ public class PageEventService {
         };
     }
 
-    @Bean
+    /*@Bean
     public Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamFunction(){
         return (input)->{
             return input
                     .filter((k,v)->v.getDuration()>100)
                     .map((k,v)->new KeyValue<>(v.getName(),0L))
                     .groupBy((k,v)->k, Grouped.with(Serdes.String(),Serdes.Long()))
-                    .count().toStream();
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(1)))
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k,v)->new KeyValue<>("=>"+k.window().startTime()+k.window().endTime()+k.key(),v));
+        };
+    }*/
+    @Bean
+    public  Function<KStream<String,PageEvent>,KStream<String,Long>> kStreamKStreamFunction() {
+        return (input) -> {
+            return input
+                    .filter((k, v) -> v.getDuration() > 100)
+                    .map((k, v) -> new KeyValue<>(v.getName(), 0L))
+                    .groupBy((k, v) -> k, Grouped.with(Serdes.String(), Serdes.Long()))
+
+                    /*
+                       windowed By here we calculate just the evolution in period of time !
+                     */
+                    .windowedBy(TimeWindows.of(Duration.ofSeconds(5)))
+                    /*
+                        create a store called page-count
+                     */
+                    .count(Materialized.as("page-count"))
+                    .toStream()
+                    .map((k, v) -> new KeyValue<>(k.key(), v));
+
         };
     }
 }
